0d8817316e6f0a7e1aec3ac7328c209c63afca50
Caroline Evans <carolineevans261@gmail.com>
2023-06-20 21:24:51 +0100

    refactored update_quality method into 5 smaller methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,52 +4,45 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality()    
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      item.quality += get_quality_change(item)
+      item.quality = 50 if item.quality > 50 
+      item.quality = 0 if item.quality < 0 
     end
+  end
+
+  def get_quality_change(item)
+    case item.name
+    when "Aged Brie"
+      brie_quality_change(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      backstage_passes_quality_change(item)
+    when "Sulfuras, Hand of Ragnaros"
+      0
+    else
+      ordinary_quality_chage(item)
+    end
+  end
+
+  def brie_quality_change(item)
+    item.sell_in > 0 ? 1 : 2
+  end
+
+  def backstage_passes_quality_change(item)
+    if item.sell_in <= 0 
+      - item.quality
+    elsif item.sell_in < 6
+      3
+    elsif item.sell_in < 11
+      2
+    else
+      1
+    end
+  end  
+
+  def ordinary_quality_chage(item)
+    item.sell_in > 0 ? -1 : -2
   end
 end
 
